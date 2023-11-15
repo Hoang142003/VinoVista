37195b50599818a85430fa41a8d66906e46a49a7
Peter <hoangnm9a2@gmail.com>
2023-11-16 01:43:21 +0700

    Thêm file mã hóa mật khẩu

--- app/src/main/java/com/example/vinovista/Activity/MatKhauMoi.java
+++ app/src/main/java/com/example/vinovista/Activity/MatKhauMoi.java
@@ -1,17 +1,29 @@
 package com.example.vinovista.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.vinovista.Activity.DangNhap;
+import com.example.vinovista.Adapter.PasswordEncoder;
 import com.example.vinovista.R;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MatKhauMoi extends AppCompatActivity {
     Button btnthaydoi;
+    EditText edt_mk, edk_nhaplaimk;
+    ProgressBar pb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,16 +33,44 @@ public class MatKhauMoi extends AppCompatActivity {
     }
 
     private void setEvent() {
-        Intent intent =new Intent(this, DangNhap.class);
+        String sdt = getIntent().getStringExtra("sdt");
         btnthaydoi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(intent);
+                pb.setVisibility(View.VISIBLE);
+                if (edt_mk.getText().toString().equals(edk_nhaplaimk.getText().toString())) {
+                    if (edt_mk.getText().toString().trim().length() >= 8) {
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("NhanVien");
+                        reference.child(sdt).child("matKhau").setValue(PasswordEncoder.encrypt(edt_mk.getText().toString().trim()), new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                                pb.setVisibility(View.GONE);
+                                if (error == null) {
+                                    Toast.makeText(MatKhauMoi.this, "Sửa mật khẩu thành công", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(MatKhauMoi.this, DangNhap.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(MatKhauMoi.this, "Sửa mật khẩu không thành công", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                    } else {
+                        pb.setVisibility(View.GONE);
+                        Toast.makeText(MatKhauMoi.this, "Mật khẩu phải ít nhất 8 ký tự", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(MatKhauMoi.this, "Mật khẩu không khớp", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
     private void setControl() {
-        btnthaydoi=findViewById(R.id.btnthaydoi);
+        btnthaydoi = findViewById(R.id.btnthaydoi);
+        edt_mk = findViewById(R.id.edt_mk);
+        edk_nhaplaimk = findViewById(R.id.edk_nhaplaimk);
+        pb = findViewById(R.id.pb);
     }
 }
