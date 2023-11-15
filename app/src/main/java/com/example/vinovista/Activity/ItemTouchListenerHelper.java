13a76c656a44fa08eea007d8d1bcf116c34bce96
baotruongmama123 <baotruongmama123@gmail.com>
2023-11-15 21:39:42 +0700

    Quản lý sản phẩm

---
+++ app/src/main/java/com/example/vinovista/Activity/ItemTouchListenerHelper.java
@@ -0,0 +1,7 @@
+package com.example.vinovista.Activity;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface ItemTouchListenerHelper {
+    void onSwiped(RecyclerView.ViewHolder viewHolder);
+}
